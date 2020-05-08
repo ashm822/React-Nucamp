610fbcab7bce6e344a9a4b8c488945598a8ea046
Ashraf Madina <ashraf.madina@asset-map.com>
2020-05-08 18:56:06 -0400

    added React Redux Form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,78 +1,61 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
-import { CAMPSITES } from '../shared/campsites';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
+import { connect } from 'react-redux';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Contact from './ContactComponent';
-import { COMMENTS } from '../shared/comments';
-import { PARTNERS } from '../shared/partners';
-import { PROMOTIONS } from '../shared/promotions';
 import About from './AboutComponent';
 
 
+const mapStateToProps = state => {
+    return {
+        campsites: state.campsites,
+        comments: state.comments,
+        partners: state.partners,
+        promotions: state.promotions
+    }
+}
+
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES,
-            comments: COMMENTS,
-            partners: PARTNERS,
-            promotions: PROMOTIONS
-        };
-    }
-
-    
 
   render() {
 
     const HomePage = () => {
         return (
-            <Home 
-                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
-                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
-                    partner={this.state.partners.filter(partner => partner.featured)[0]}          
-            />
-                
-        );
-    }
-
-    const AboutPage = () => {
-        return (
-            <About
-            partners={this.state.partners}
+            <Home
+                campsite={this.props.campsites.filter(campsite => campsite.featured)[0]}
+                promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
+                partner={this.props.partners.filter(partner => partner.featured)[0]}
             />
         );
-    }
+    };
 
-      
     const CampsiteWithId = ({match}) => {
         return (
-            <CampsiteInfo 
-                campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-                comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
-            />
+            <CampsiteInfo campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]} 
+              comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} />
         );
-    };    
+    };
 
-      return (
-          <div>             
-                <Header />
-                <Switch>
-                    <Route path='/home' component={HomePage} />
-                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
-                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
-                    <Route path='/aboutus' component={AboutPage} />
-                    <Route exact path='/contactus' component={Contact} />
-                    <Redirect to='/home' />
-                </Switch>
-                <Footer />
-          </div>
-      );
+    return (
+        <div>
+            <Header />
+            <Switch>
+                <Route path='/home' component={HomePage} />
+                <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
+                <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+                <Route exact path='/contactus' component={Contact} />
+                <Route exact path='/aboutus' render={() => <About partners={this.props.partners} /> } />
+                <Redirect to='/home' />
+            </Switch>
+            <Footer />
+        </div>
+    );
   }
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
